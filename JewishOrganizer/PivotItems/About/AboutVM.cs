0b28095b2a42d1891454b8b6253f47013cb19097
Alexander <khimalex@live.com>
2017-11-28 10:32:11 +0300

    Added "About" page, some reorganizations, removed unused "usings".

---
+++ JewishOrganizer/PivotItems/About/AboutVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.ApplicationModel;
+
+namespace JewishOrganizer.PivotItems
+{
+	public class AboutVM : ViewModelBase
+	{
+		public String Version
+		{
+			get
+			{
+				var version = Package.Current.Id.Version;
+				return $"Версия: {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+			}
+		}
+
+	}
+}
